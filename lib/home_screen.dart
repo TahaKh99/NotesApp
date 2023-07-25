b4497c0c534b88ca6091652f4a7b351d2b3a005c
Taha Khamis <Tahakhamis1999@gmail.com>
2023-07-26 00:15:05 +0800

    Task B2 completed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,8 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:map_exam/edit_screen.dart';
+import 'package:map_exam/helpers/utils.dart';
+import 'package:map_exam/note.dart';
 
-class HomeScreen extends StatelessWidget {
-  static Route route() => MaterialPageRoute(builder: (_) => const HomeScreen());
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String userID = FirebaseAuth.instance.currentUser!.uid;
+
+
+
+
+  Future<List<Note>> fetchNotes() async {
+    String userID = FirebaseAuth.instance.currentUser!.uid;
+
+    QuerySnapshot<Map<String, dynamic>> snapshot =
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(userID)
+        .collection("notes")
+        .get();
+
+    List<Note> notes = snapshot.docs.map((doc) {
+      var note = Note.fromJson(doc.data());
+      note.id = doc.id;
+      return note;
+    }).toList();
+
+    return notes;
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -10,66 +45,123 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('My Notes'),
         actions: [
-          CircleAvatar(
-            backgroundColor: Colors.blue.shade200,
-            child: const Text(
-              '4',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22.0),
-            ),
-          ),
-          const SizedBox(
-            width: 10,
-          ),
+          _buildNoteCount(context),
         ],
       ),
-      body: ListView.separated(
-        itemCount: 4,
-        separatorBuilder: (context, index) => const Divider(
-          color: Colors.blueGrey,
-        ),
-        itemBuilder: (context, index) => ListTile(
-          trailing: SizedBox(
-            width: 110.0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.edit, color: Colors.blue),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: const Icon(
-                    Icons.delete,
-                    color: Colors.blue,
-                  ),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ),
-          title: const Text('Note title'),
-          subtitle: const Text('Note content'),
-          onTap: () {},
-          onLongPress: () {},
-        ),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
+      body: ListView(
+
         children: [
-          FloatingActionButton(
-              child: const Icon(Icons.menu),
-              tooltip: 'Show less. Hide notes content',
-              onPressed: () {}),
-
-          /* Notes: for the "Show More" icon use: Icons.menu */
-
-          FloatingActionButton(
-            child: const Icon(Icons.add),
-            tooltip: 'Add a new note',
-            onPressed: () {},
-          ),
+          _buildNoteList(),
         ],
       ),
+      drawer: Utils.buildDrawer(context),
+      floatingActionButton: _buildFloatingActionButtons(context),
+    );
+  }
+
+  Widget _buildNoteCount(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('users').doc(userID).collection("notes").snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const SizedBox();
+        }
+
+        final noteCount = snapshot.data?.docs.length ?? 0;
+        return Padding(
+          padding: const EdgeInsets.only(right: 15.0),
+          child: CircleAvatar(
+            backgroundColor: Colors.blue.shade200,
+            child: Text(
+              '$noteCount',
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 22.0),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+
+
+  Widget _buildNoteList() {
+    return FutureBuilder<List<Note>>(
+      future: fetchNotes(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        final notes = snapshot.data;
+        if (notes == null || notes.isEmpty) {
+          return const Center(child: Text('No notes available.'));
+        }
+
+        return ListView.builder(
+          shrinkWrap: true,
+          itemCount: notes.length,
+          itemBuilder: (context, index) {
+            var note = notes[index];
+            bool isLastNote = index == notes.length - 1;
+
+            return Column(
+              children: [
+                ListTile(
+
+                  title: Text(note.title ?? ''),
+                  subtitle: Text(note.content ?? ''),
+
+                  onTap: () {
+
+                  },
+                  onLongPress: () {
+
+                    },
+                ),
+                if (!isLastNote)
+                  const Divider( // Add a divider after each ListTile except for the last one
+                    thickness: 1,
+                  ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+
+
+  Widget _buildFloatingActionButtons(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        FloatingActionButton(
+          heroTag: "showDetails",
+          key: UniqueKey(), // Unique key for the first FloatingActionButton
+          child: Icon(Icons.close_fullscreen),
+          tooltip: 'Show less. Hide notes content',
+          onPressed: () {
+
+          },
+        ),
+        const SizedBox(width: 16), // Add some space between the FloatingActionButtons
+        FloatingActionButton(
+          heroTag: "Edit",
+          key: UniqueKey(), // Unique key for the second FloatingActionButton
+          child: const Icon(Icons.add),
+          tooltip: 'Add a new note',
+          onPressed: () {
+            // Navigate to the EditScreen to add a new note
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => const EditScreen(),
+              ),
+            );
+          },
+        ),
+      ],
     );
   }
 }
